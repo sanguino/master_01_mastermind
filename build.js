f8549de86a5b0fb6064f1e7ccd0043685b9a1440
Miguel G. Sanguino <sanguino@me.com>
2020-10-09 15:30:44 +0200

    jpg

--- build.js
+++ build.js
@@ -2,4 +2,4 @@ var plantuml = require('node-plantuml');
 var fs = require('fs');
  
 var gen = plantuml.generate("clases.puml");
-gen.out.pipe(fs.createWriteStream("clases.png"));
+gen.out.pipe(fs.createWriteStream("clases.jpg"));
