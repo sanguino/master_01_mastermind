9e70240dc7dd14b24ef95e7bed0b5f44005838d2
Miguel G. Sanguino <sanguino@me.com>
2020-10-09 15:30:44 +0200

    rename files

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 var plantuml = require('node-plantuml');
 var fs = require('fs');
  
-var gen = plantuml.generate("clases.puml");
-gen.out.pipe(fs.createWriteStream("clases.png"));
+var gen = plantuml.generate("domainModel.puml");
+gen.out.pipe(fs.createWriteStream("domainModel.jpg"));
+console.log("domainModel.jpg generated")
