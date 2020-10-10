418d49443b7528685d9da536d3ceb99049e9286a
Miguel G. Sanguino <sanguino@me.com>
2020-10-10 19:21:27 +0200

    My Classes uml proposal

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var plantuml = require('node-plantuml');
 var fs = require('fs');
 
-var gen = plantuml.generate("domainModel.puml");
-gen.out.pipe(fs.createWriteStream("domainModel.jpg"));
-console.log("domainModel.jpg generated")
+var gen = plantuml.generate("clases.puml");
+gen.out.pipe(fs.createWriteStream("clases.jpg"));
+console.log("clases.jpg generated")
